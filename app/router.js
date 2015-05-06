e4c67786e0ddcef6970b2fd2557d59051b5fc314
Oskar Olsson <oskar.ols@gmail.com>
2015-05-06 13:25:07 +0200

    Added route configuration

--- app/router.js
+++ app/router.js
@@ -6,4 +6,7 @@ var Router = Ember.Router.extend({
 });
 
 export default Router.map(function() {
+    this.resource('products', {path: '/'}, function() {
+        this.resource('product', {path: '/:id'})
+    });
 });
