de1a46ea6ebd4e8d19f8e2f1a62f5f399f172df6
Oskar Olsson <oskar.ols@gmail.com>
2015-05-06 15:12:31 +0200

    Using transition via actions for the product item list

--- app/routes/products/index.js
+++ app/routes/products/index.js
@@ -1,5 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-	controllerName: 'products'
+	controllerName: 'products',
+
+	actions: {
+		goToProduct: function(product) {
+			this.transitionTo('product', product);
+		}
+	}
 });
